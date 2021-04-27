46271af08234f2886deabfd0809dcc1ef199d0ab
Patrick Dinklage <pdinklag@gmail.com>
2021-04-27 07:49:07 +0200

    Implement Projectile lifetime

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,4 +6,18 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Projectile : MonoBehaviour
 {
+    [Tooltip("The number of seconds after which this projectile will automatically destroy itself.")]
+    public float Lifetime = 3.0f;
+
+    private void Update()
+    {
+        // reduce lifetime by the time (in seconds) that has passed since the last Update
+        Lifetime -= Time.deltaTime;
+
+        if(Lifetime <= 0)
+        {
+            // the lifetime has expired - destroy the projectile!
+            Destroy(gameObject);
+        }
+    }
 }
