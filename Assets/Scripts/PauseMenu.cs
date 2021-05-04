1fbf43568f57414738f909cc2f2521df932743ff
Patrick Dinklage <pdinklag@gmail.com>
2021-05-04 11:10:04 +0200

    Implement Exit to Main Menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// Implements pause menu logic.
 /// </summary>
 public class PauseMenu : MonoBehaviour
 {
+    [Tooltip("The scene to load when exiting to the main menu.")]
+    public string MainMenuSceneName;
+
     public void OnBackToGameClicked()
     {
         Level.Instance.PauseGame(false);
@@ -12,6 +16,7 @@ public class PauseMenu : MonoBehaviour
 
     public void OnExitToMainMenuClicked()
     {
-        // TODO
+        Time.timeScale = 1.0f; // un-pause!
+        SceneManager.LoadScene(MainMenuSceneName);
     }
 }
