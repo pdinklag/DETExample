c70bcc112ee95d4951ff124641da849e4621152d
Patrick Dinklage <pdinklag@gmail.com>
2021-05-04 09:00:25 +0200

    Allow MovableWall to react to ray weapon hits; make animation relative to parent

--- Assets/Scripts/MovableWall.cs
+++ Assets/Scripts/MovableWall.cs
@@ -4,40 +4,44 @@ using UnityEngine;
 /// <summary>
 /// A wall that moves down when shot.
 /// </summary>
-[RequireComponent(typeof(Animator))]
-public class MovableWall : MonoBehaviour
+public class MovableWall : MonoBehaviour, RayWeapon.HitListener
 {
     private static readonly string TRIGGER_MOVE_DOWN = "MoveDown";
     private static readonly string TRIGGER_MOVE_UP = "MoveUp";
 
+    [Tooltip("The animator to use for animation.")]
+    public Animator Animator;
+
     [Tooltip("The weapon that must be used to trigger the wall.")]
     public string TriggerProjectileID = "RedMarble";
+
+    [Tooltip("The ray weapon that must be used to trigger the wall.")]
+    public RayWeapon TriggerRayWeapon;
 
     [Tooltip("The number of seconds that the wall stays down.")]
     public float DownDuration = 3.0f;
 
-    private Animator _animator;
     private bool _moving;
 
-    private void Start()
-    {
-        _animator = GetComponent<Animator>();
-    }
-
-    private void OnCollisionEnter(Collision collision)
+    private void Trigger()
     {
         // make sure we are not already moving
         if (!_moving)
         {
-            // test if the hitting collider is a projectile
-            if (collision.collider.TryGetComponent<Projectile>(out var proj))
+            // start the trigger coroutine
+            StartCoroutine(CoTrigger());
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // test if the hitting collider is a projectile
+        if (collision.collider.TryGetComponent<Projectile>(out var proj))
+        {
+            // if so, make sure it is the correct type of projectile
+            if (proj.ID == TriggerProjectileID)
             {
-                // if so, make sure it is the correct type of projectile
-                if (proj.ID == TriggerProjectileID)
-                {
-                    // start the trigger coroutine
-                    StartCoroutine(CoTrigger());
-                }
+                Trigger();
             }
         }
     }
@@ -48,24 +52,33 @@ public class MovableWall : MonoBehaviour
         _moving = true;
 
         // fire the "MoveDown" trigger in the Animator state machine
-        _animator.SetTrigger(TRIGGER_MOVE_DOWN);
+        Animator.SetTrigger(TRIGGER_MOVE_DOWN);
 
         // wait before moving back up
         yield return new WaitForSeconds(DownDuration);
 
         // fire the "MoveUp" trigger
-        _animator.SetTrigger(TRIGGER_MOVE_UP);
+        Animator.SetTrigger(TRIGGER_MOVE_UP);
 
         // wait until the transition is completed
         do
         {
             yield return null;
-        } while (_animator.IsInTransition(0));
+        } while (Animator.IsInTransition(0));
         // EXERCISE: what happens if we turn the loop above into a head-controlled loop rather than a tail-controlled loop?
 
         // reset triggers and moving flag
-        _animator.ResetTrigger(TRIGGER_MOVE_UP);
-        _animator.ResetTrigger(TRIGGER_MOVE_DOWN);
+        Animator.ResetTrigger(TRIGGER_MOVE_UP);
+        Animator.ResetTrigger(TRIGGER_MOVE_DOWN);
         _moving = false;
+    }
+
+    public void OnHitByRayWeapon(RayWeapon weapon, PlayerCharacter instigator, RaycastHit hit)
+    {
+        // test if hit by the correct ray weapon
+        if (weapon == TriggerRayWeapon)
+        {
+            Trigger();
+        }
     }
 }
